bac4447fddb010ba28fe9932085f79d885b12798
bajradeeponsaha <bajradeeponsaha5@gmail.com>
2023-07-03 21:14:56 +0530

    Structuring code

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='main-header'>
+        <div className="logo-header">
+            <img src="" alt="" />
+        </div>
+        <div className="searchbar-header"></div>
+        <div className="userhandle-header"></div>
+    </header>
+  )
+}
+
+export default Header
