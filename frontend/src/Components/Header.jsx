94161957ad77b312e85e8c4ebf3fbc43c4ce3216
bajradeeponsaha <bajradeeponsaha5@gmail.com>
2023-07-04 22:50:17 +0530

    Making Header Section

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import "./Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faBars, faCircleUser, faLocationDot, faCaretDown, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
 
 const Header = () => {
   return (
     <header className='main-header'>
+        {/* Logo Section */}
         <div className="logo-header">
-            <img src="" alt="" />
+            <h4>dropshoptail</h4>
         </div>
-        <div className="searchbar-header"></div>
-        <div className="userhandle-header"></div>
+
+        {/* Searchbar Section */}
+        <div className="searchbar-header">
+          <div className="search-location">
+            <FontAwesomeIcon icon={faLocationDot} style={{color: "#ce1212",}} />
+            <input type="text" />
+            <FontAwesomeIcon icon={faCaretDown} style={{color: "#39d05f",}} />
+          </div>
+          <div className="vertical-bar"></div>
+          <div className="query-search">
+            <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#36ce64",}} />
+            <input type='text' />
+          </div>
+        </div>
+
+        {/* User Handle Section */}
+        <div className="userhandle-header">
+          <div className="cart-header">
+            <FontAwesomeIcon icon={faCartShopping} style={{color: "#77bb3f",}} id='logo' />
+          </div>
+          <div className="user-access">
+            <FontAwesomeIcon icon={faCircleUser} style={{color: "#4fabb5",}} />
+            <FontAwesomeIcon icon={faBars} style={{color: "#4fabb5",}} />
+          </div>
+        </div>
     </header>
   )
 }
