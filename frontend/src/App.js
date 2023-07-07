665948d9c2232eba6a473768d4cf1bf51109f59d
bajradeeponsaha <bajradeeponsaha5@gmail.com>
2023-07-07 21:52:57 +0530

    installed bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import Header from './Components/Header';
+
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <button className="btn btn-success">SAVE</button>
     </div>
   );
 }
